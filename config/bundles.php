c9bb53e4bb1ee9b726889bb87400ddeb330c1fed
Vidy Videni <vidy.videni@gmail.com>
2019-10-27 10:51:34 +0800

    show non-existent parameter container.build_id bug

--- config/bundles.php
+++ config/bundles.php
@@ -5,5 +5,7 @@ return [
     Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Rollerworks\Bundle\PasswordStrengthBundle\RollerworksPasswordStrengthBundle::class => ['all' => true],
 ];
